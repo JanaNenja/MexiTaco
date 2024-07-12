22de9dc08c916c54a5e1a860bd6639b853985538
Jana Baqays <134169601+JanaNenja@users.noreply.github.com>
2024-07-13 02:54:46 +0300

    First Commit
    MexiTaco restaurant is an online website for Mexican food. Customers can make food orders through the website. The services provided are not only food orders, there is information about Mexican food, location of the restaurant, restaurant menu organized by categories (breakfast, boxes, drinks and desserts), feedback form to gather information from the customers whether they like the services or need to improve, also further contact information is listed at the end of the website such as social media and emails.

---
+++ myscript.js
@@ -0,0 +1,7 @@
+function writeText(txt) {
+    document.getElementById("desc").innerHTML = txt;
+}
+
+function clearText() {
+    document.getElementById("desc").innerHTML = "Mouse over the circles and see the different descriptions.";
+}
